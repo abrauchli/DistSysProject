6b94f3e6ddbd313668d9308283b12a49959c85aa
helandre <helandre@ethz.ch>
2011-11-15 17:18:43 +0100

    Client: initiated android project

---
+++ client/SurvivalGuide/src/ch/ethz/inf/vs/android/g54/a4/SurvivalGuideActivity.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.android.g54.a4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SurvivalGuideActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
