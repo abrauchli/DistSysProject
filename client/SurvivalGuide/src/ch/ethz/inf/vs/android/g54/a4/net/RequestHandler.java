5e9b9af152b1347559b1f417e49ad26b5df86824
helandre <helandre@ethz.ch>
2011-11-15 21:43:16 +0100

    added support for post request (getnearestlocation)

--- client/SurvivalGuide/src/ch/ethz/inf/vs/android/g54/a4/net/RequestHandler.java
+++ client/SurvivalGuide/src/ch/ethz/inf/vs/android/g54/a4/net/RequestHandler.java
@@ -5,6 +5,7 @@ import java.io.*;
 import org.apache.http.*;
 import org.apache.http.client.*;
 import org.apache.http.client.methods.*;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.*;
 import org.json.*;
 
@@ -16,25 +17,61 @@ public class RequestHandler {
 	private static final String PROTO_PREFIX = "http://";
 	private static final String HOST = "deserver.moeeeep.com";
 	private static final int PORT = 23032;
-	
-	private static final String LOC_ROOMS = "/r/";
-	private static final String LOC_MACS = "/m/";
-	
+
+	private static final String ROOMS = "/r/";
+	private static final String MACS = "/m/";
+	private static final String LOCATION = "/getNearestLocation/";
+
 	private static final String TAG = "SG_NetLib";
-	
+
 	private Context context;
-	
-	public RequestHandler(Context context){
+
+	public RequestHandler(Context context) {
 		this.context = context;
 	}
-	
+
 	private JSONObject getRequest(String resourceLoc) {
 		HttpClient client = new DefaultHttpClient();
 		HttpGet get = new HttpGet(PROTO_PREFIX + HOST + ":" + PORT + resourceLoc);
 		get.addHeader("Accept", "application/json");
 		JSONObject jsonResponse = null;
+		HttpResponse response;
 		try {
-			HttpResponse response = client.execute(get);
+			response = client.execute(get);
+			jsonResponse = parseResponse(response);
+		} catch (ClientProtocolException e) {
+			Toast.makeText(context, "ClientProtocolException", Toast.LENGTH_LONG).show();
+			Log.e(TAG, e.toString());
+		} catch (IOException e) {
+			Toast.makeText(context, "IOException", Toast.LENGTH_LONG).show();
+			Log.e(TAG, e.toString());
+		}
+		return jsonResponse;
+	}
+
+	private JSONObject postRequest(String resourceLoc, JSONObject jsonRequest) {
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(PROTO_PREFIX + HOST + ":" + PORT + resourceLoc);
+		post.addHeader("Content-Type", "application/json");
+		JSONObject jsonResponse = null;
+		try {
+			StringEntity se = new StringEntity(jsonRequest.toString());
+			post.setEntity(se);
+			HttpResponse response = client.execute(post);
+			jsonResponse = parseResponse(response);
+		} catch (ClientProtocolException e) {
+			Toast.makeText(context, "ClientProtocolException", Toast.LENGTH_LONG).show();
+			Log.e(TAG, e.toString());
+		} catch (IOException e) {
+			Toast.makeText(context, "IOException", Toast.LENGTH_LONG).show();
+			Log.e(TAG, e.toString());
+		}
+		return jsonResponse;
+	}
+
+	private JSONObject parseResponse(HttpResponse response) {
+		JSONObject jsonResponse = null;
+		try {
 			HttpEntity entity = response.getEntity();
 			if (entity != null) {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
@@ -49,32 +86,33 @@ public class RequestHandler {
 					Log.e(TAG, e.toString());
 				}
 			}
-		} catch (ClientProtocolException e) {
-			Toast.makeText(context, "ClientProtocolException", Toast.LENGTH_LONG).show();
-			Log.e(TAG, e.toString());
 		} catch (IOException e) {
 			Toast.makeText(context, "IOException", Toast.LENGTH_LONG).show();
 			Log.e(TAG, e.toString());
 		}
 		return jsonResponse;
 	}
-	
-	public JSONObject getAllBuildings(){
-		return getRequest(LOC_ROOMS);
+
+	public JSONObject getAllBuildings() {
+		return getRequest(ROOMS);
 	}
-	
-	public JSONObject getBuilding(String building){
-		String resourceLocation = String.format("%s%s", LOC_ROOMS, building);
+
+	public JSONObject getBuilding(String building) {
+		String resourceLocation = String.format("%s%s", ROOMS, building);
 		return getRequest(resourceLocation);
 	}
-	
-	public JSONObject getFloor(String building, String floor){
-		String resourceLocation = String.format("%s%s/%s", LOC_ROOMS, building, floor);
+
+	public JSONObject getFloor(String building, String floor) {
+		String resourceLocation = String.format("%s%s/%s", ROOMS, building, floor);
 		return getRequest(resourceLocation);
 	}
-	
-	public JSONObject getRoom(String building, String floor, String roomNumber){
-		String resourceLocation = String.format("%s%s/%s/%s", LOC_ROOMS, building, floor, roomNumber);
+
+	public JSONObject getRoom(String building, String floor, String roomNumber) {
+		String resourceLocation = String.format("%s%s/%s/%s", ROOMS, building, floor, roomNumber);
 		return getRequest(resourceLocation);
+	}
+
+	public JSONObject getLocation(JSONObject wifiReadings) {
+		return postRequest(LOCATION, wifiReadings);
 	}
 }
